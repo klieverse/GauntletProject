b082ef6bbba1b6fa731a86b518368d7f1beb3084
klieverse <58080050+klieverse@users.noreply.github.com>
2019-11-27 13:18:21 +0100

    Hopelijk werkt het nu

---
+++ Gauntlet+/Gauntlet+/GameObjects/Button.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+
+class Button : SpriteGameObject
+{
+    protected bool pressed;
+    Color backgroundcolor = Color.Gray;
+
+    public Button(string imageAsset, int layer = 0, string id = "")
+        : base(imageAsset, layer, id)
+    {
+        pressed = false;
+    }
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        pressed = inputHelper.MouseLeftButtonPressed() &&
+            BoundingBox.Contains((int)inputHelper.MousePosition.X, (int)inputHelper.MousePosition.Y);
+        if (BoundingBox.Contains((int)inputHelper.MousePosition.X, (int)inputHelper.MousePosition.Y))
+        {
+            backgroundcolor = Color.LightGray;
+        }
+        else
+        {
+            backgroundcolor = Color.Gray;
+        }
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+        pressed = false;
+    }
+
+    public bool Pressed
+    {
+        get { return pressed; }
+    }
+}
---
+++ Gauntlet+/Gauntlet+/Level/Tile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+enum TileType
+{
+    Background,
+    Exit,
+    Wall,
+    BreakableWall,
+    Teleporter,
+    Door,
+    Trap
+}
+
+class Tile : SpriteGameObject
+{
+    protected TileType type;
+
+    public Tile(string assetname = "", TileType tp = TileType.Background, int layer = 0, string id = "")
+        : base(assetname, layer, id)
+    {
+        type = tp;
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        if (type == TileType.Background)
+        {
+            return;
+        }
+        base.Draw(gameTime, spriteBatch);
+    }
+
+    public TileType TileType
+    {
+        get { return type; }
+    }
+    
+}
+
