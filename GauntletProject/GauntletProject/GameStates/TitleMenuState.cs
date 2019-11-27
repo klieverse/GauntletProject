d5310463e0294e33811dc45e8f310d573c9213c4
klieverse <58080050+klieverse@users.noreply.github.com>
2019-11-27 13:06:42 +0100

    Blueprint

---
+++ GauntletProject/GauntletProject/GameStates/TitleMenuState.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+
+class TitleMenuState : GameObjectList
+{
+    protected Button singlePlayerButton, multiPlayerButton, helpButton;
+
+    public TitleMenuState()
+    {
+        // load the title screen
+        SpriteGameObject titleScreen = new SpriteGameObject("Backgrounds/spr_title", 0, "background");
+        Add(titleScreen);
+
+        // add a single player button
+        singlePlayerButton = new Button("Sprites/Button", 1);
+        singlePlayerButton.Position = new Vector2((GameEnvironment.Screen.X - singlePlayerButton.Width) / 2, 440);
+        Add(singlePlayerButton);
+
+        //add a multi player button
+        multiPlayerButton = new Button("Sprites/Button", 1);
+        multiPlayerButton.Position = new Vector2((GameEnvironment.Screen.X - singlePlayerButton.Width) / 2, 540);
+        Add(singlePlayerButton);
+
+        // add a help button
+        helpButton = new Button("Sprites/Button", 1);
+        helpButton.Position = new Vector2((GameEnvironment.Screen.X - helpButton.Width) / 2, 600);
+        Add(helpButton);
+    }
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        base.HandleInput(inputHelper);
+        if (singlePlayerButton.Pressed)
+        {
+            GameEnvironment.GameStateManager.SwitchTo("singlePlayerState");
+        }
+        else if(multiPlayerButton.Pressed)
+        {
+            GameEnvironment.GameStateManager.SwitchTo("multiPlayerState");
+        }
+        else if (helpButton.Pressed)
+        {
+            GameEnvironment.GameStateManager.SwitchTo("helpState");
+        }
+    }
+}
---
+++ GauntletProject/GauntletProject/Level/LevelLoading.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+partial class Level : GameObjectList
+{
+    public void LoadTiles(string path)
+    {
+        List<string> textLines = new List<string>();
+        StreamReader fileReader = new StreamReader(path);
+        string line = fileReader.ReadLine();
+        int width = line.Length;
+        while (line != null)
+        {
+            textLines.Add(line);
+            line = fileReader.ReadLine();
+        }
+        TileField tiles = new TileField(textLines.Count - 1, width, 1, "tiles");
+        
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < textLines.Count - 1; ++y)
+            {
+                Tile t = LoadTile(textLines[y][x], x, y);
+                tiles.Add(t, x, y);
+            }
+        }
+    }
+
+    private Tile LoadTile(char tileType, int x, int y)
+    {
+        return new Tile();
+        //switch (tileType)
+        //{
+            
+        //}
+    }
+
+    /*private Tile LoadBasicTile()
+    {
+        Tile t = new Tile("Tiles/" + name, tileType);
+        
+    }*/
+}
