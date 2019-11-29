6186bec59031c463dcea4050d73d4824ea690e4b
TimoBlok <57986353+TimoBlok@users.noreply.github.com>
2019-11-29 17:41:12 +0100

    Added playerobject class with constructor and list of players

---
+++ Gauntlet+/Gauntlet+/GameObjects/PlayerObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauntlet_.GameObjects
+{
+    class PlayerObject : AnimatedGameObject
+    {
+        protected List<PlayerObject> players;
+
+        public PlayerObject(int layer = 0, string id = "")
+        : base(layer, id)
+        {
+            players = new List<PlayerObject>();
+        }
+    }
+}
