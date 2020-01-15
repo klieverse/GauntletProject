01ab898a42181704b3f309fac8067bd7cc7540de
Shikasi <50450951+Shikasi@users.noreply.github.com>
2020-01-15 15:27:42 +0100

    Update Level.cs

--- Gauntlet+/Gauntlet+/Level/Level.cs
+++ Gauntlet+/Gauntlet+/Level/Level.cs
@@ -21,6 +21,7 @@ partial class Level : GameObjectList
         Add(new GameObjectList(1, "items"));
         Add(new GameObjectList(3,"playershot"));
         Add(new GameObjectList(3,"enemieShot"));
+        Add(new GameObjectList(2, "teleport"));
 
        // Add(new Questor(2, "Elf", new Vector2(150, 150), this, true));
        // Add(new PlayerStatField("Elf"));
