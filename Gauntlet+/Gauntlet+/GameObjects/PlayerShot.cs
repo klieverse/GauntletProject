36babf92887e5f226db2241f75e0355429111b4b
Kayleigh Lieverse <k.z.lieverse@students.uu.nl>
2020-01-26 19:11:19 +0100

    Playershots

--- Gauntlet+/Gauntlet+/GameObjects/PlayerShot.cs
+++ Gauntlet+/Gauntlet+/GameObjects/PlayerShot.cs
@@ -10,19 +10,45 @@ class PlayerShot : SpriteGameObject
 {
     float shotSpeed, baseShotSpeed = 250;
     float shotStrength;
-    Player player;
+    public Player player
+    {
+        get;
+        set;
+    }
 
 
     public PlayerShot(string id, float shotSpeed, float shotStrength, Vector2 direction, Vector2 position, Player player, InputHelper inputHelper) : base(assetName: id + "Shot", layer: 0, id, sheetIndex: 0)
     {
         velocity = direction;
         this.position.X = position.X;
-        this.position.Y = position.Y - player.Height / 3;
+        if(player != null)
+        {
+            this.position.Y = position.Y - player.Height / 3;
+            this.player = player;
+        }
+        else
+        {
+            this.player = null;
+            this.position.Y = position.Y;
+        }
+        
         this.shotSpeed = shotSpeed;
         this.shotStrength = shotStrength;
-        this.player = player;
-        HandleDirection(inputHelper);
-        origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+
+        if (inputHelper != null)
+        {
+            HandleDirection(inputHelper);
+        }
+        
+        if(sprite != null)
+        {
+            origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+        }
+        else
+        {
+            origin = Vector2.Zero;
+        }
+        
     }
 
     void HandleDirection(InputHelper inputHelper) // rotates the sprite to the right direction, based on the direction it is going and giving it the right position and velocity;
@@ -193,6 +219,62 @@ class PlayerShot : SpriteGameObject
         {
             visible = false;
         }
+    }
+
+    public void HandleIncoming()
+    {
+        this.position.Y = position.Y - player.Height / 3;
+        origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+        if (velocity.X > 0 && velocity.Y == 0) // facing right
+        {
+            velocity.X = shotSpeed * 75 + baseShotSpeed;
+            Rotate(90);
+        }
+        if (velocity.X > 0 && velocity.Y > 0) // facing down right
+        {
+            velocity.X = 0.71f * (shotSpeed * 75 + baseShotSpeed);
+            velocity.Y = 0.71f * (shotSpeed * 75 + baseShotSpeed);
+            Rotate(135);
+        }
+        if (velocity.X < 0 && velocity.Y < 0) // facing up left
+        {
+            velocity.X = -0.71f * (shotSpeed * 75 + baseShotSpeed);
+            velocity.Y = -0.71f * (shotSpeed * 75 + baseShotSpeed);
+            Rotate(315);
+        }
+        if (velocity.X == 0 && velocity.Y > 0) // facing down
+        {
+            velocity.Y = (shotSpeed * 75 + baseShotSpeed);
+            Rotate(180);
+        }
+        if (velocity.X < 0 && velocity.Y == 0) // facing left
+        {
+            velocity.X = -(shotSpeed * 75 + baseShotSpeed);
+            Rotate(270);
+        }
+        if (velocity.X == 0 && velocity.Y < 0) // facing up
+        {
+            velocity.Y = -(shotSpeed * 75 + baseShotSpeed);
+            Rotate(0);
+        }
+        if (velocity.X > 0 && velocity.Y < 0) // facing up right
+        {
+            velocity.X = 0.71f * (shotSpeed * 75 + baseShotSpeed);
+            velocity.Y = -0.71f * (shotSpeed * 75 + baseShotSpeed);
+            Rotate(45);
+        }
+        if (velocity.X < 0 && velocity.Y > 0) // facing down left
+        {
+            velocity.X = -0.71f * (shotSpeed * 75 + baseShotSpeed);
+            velocity.Y = 0.71f * (shotSpeed * 75 + baseShotSpeed);
+            Rotate(225);
+        }
+    }
+
+    public bool IsShot
+    {
+        get { return true; }
+
     }
 }
 
--- Gauntlet+/Gauntlet+/Level/LevelConnection.cs
+++ Gauntlet+/Gauntlet+/Level/LevelConnection.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 
 partial class Level : GameObjectList
 {
+    string jplayer;
 
     public void SendInformation()
     {
@@ -16,10 +17,10 @@ partial class Level : GameObjectList
         Player player = players.Find(GameEnvironment.SelectedClass) as Player;
         SpriteSheet sprite = player.Sprite;
         player.SetSprite(null);
-        string jplayer = JsonConvert.SerializeObject(player);
+        jplayer = JsonConvert.SerializeObject(player);
         GameEnvironment.Connection.Send(jplayer);
         player.SetSprite(sprite);
-        
+        /*
         //sends all in game enemies
         GameObjectList enemies = GameWorld.Find("enemies") as GameObjectList;
         foreach (EnemyObject enemy in enemies.Children)
@@ -29,17 +30,24 @@ partial class Level : GameObjectList
             string jenemy = JsonConvert.SerializeObject(enemy);
             GameEnvironment.Connection.Send(jenemy);
             enemy.SetSprite(sprite);
-        }
+        }*/
         
         //sends all in game player shots
         GameObjectList playershots = GameWorld.Find("playershot") as GameObjectList;
         foreach (PlayerShot playerShot in playershots.Children)
         {
-            sprite = playerShot.Sprite;
-            playerShot.SetSprite(null);
-            string jplayershot = JsonConvert.SerializeObject(playerShot);
-            GameEnvironment.Connection.Send(jplayershot);
-            playerShot.SetSprite(sprite);
+            if(playerShot.Id == GameEnvironment.SelectedClass)
+            {
+                sprite = playerShot.Sprite;
+                Player _player = playerShot.player;
+                playerShot.player = null;
+                playerShot.SetSprite(null);
+                string jplayershot = JsonConvert.SerializeObject(playerShot);
+                GameEnvironment.Connection.Send(jplayershot);
+                playerShot.SetSprite(sprite);
+                playerShot.player = _player;
+            }
+            
         }
 
         //sends all in game enemy shots
@@ -59,50 +67,80 @@ partial class Level : GameObjectList
         if (message.Contains("playerClass"))
         {
             GameObjectList players = GameWorld.Find("players") as GameObjectList;
-            if (message.Contains("Elf"))
+            if (message.Contains("Elf") && GameEnvironment.SelectedClass != "Elf")
             {
                 Player player = players.Find("Elf") as Player;
                 SpriteSheet sprite = player.Sprite;
-                player = JsonConvert.DeserializeObject<Player>(message);
+                Questor newPlayer = JsonConvert.DeserializeObject<Questor>(message);
+                PlayerUpdate(player, newPlayer);
+                player.SetSprite(sprite);
+
                 player.SetSprite(sprite);
             }
-            else if (message.Contains("Wizard"))
+            else if (message.Contains("Wizard") && GameEnvironment.SelectedClass != "Wizard")
             {
                 Player player = players.Find("Wizard") as Player;
                 SpriteSheet sprite = player.Sprite;
-                player = JsonConvert.DeserializeObject<Player>(message);
+                Merlin newPlayer = JsonConvert.DeserializeObject<Merlin>(message);
+                PlayerUpdate(player, newPlayer);
                 player.SetSprite(sprite);
             }
-            else if (message.Contains("Warrior"))
+            else if (message.Contains("Warrior") && GameEnvironment.SelectedClass != "Warrior")
             {
                 Player player = players.Find("Warrior") as Player;
                 SpriteSheet sprite = player.Sprite;
-                player = JsonConvert.DeserializeObject<Player>(message);
+                Thor newPlayer = JsonConvert.DeserializeObject<Thor>(message);
+                PlayerUpdate(player, newPlayer);
                 player.SetSprite(sprite);
             }
-            else if (message.Contains("Valkery"))
+            else if (message.Contains("Valkery") && GameEnvironment.SelectedClass != "Valkery")
             {
                 Player player = players.Find("Valkery") as Player;
                 SpriteSheet sprite = player.Sprite;
-                player = JsonConvert.DeserializeObject<Player>(message);
+                Thyra newPlayer = JsonConvert.DeserializeObject<Thyra>(message);
+                PlayerUpdate(player, newPlayer);
                 player.SetSprite(sprite);
             }
         }
         
-        //statfield updated
 
         //enemies updated
 
+
         //playershots updated
-
-        //doors updated
-
-        //breakable walls updated
+        if(message.Contains("IsShot"))
+        {
+            Console.WriteLine(message);
+            PlayerShot shot = JsonConvert.DeserializeObject<PlayerShot>(message);
+            if(shot.Id != GameEnvironment.SelectedClass && shot.Visible == true)
+            {
+                SpriteSheet shotsprite = new SpriteSheet(shot.Id + "Shot");
+                shot.SetSprite(shotsprite);
+                GameObjectList players = GameWorld.Find("players") as GameObjectList;
+                Player player = players.Find(shot.Id) as Player;
+                shot.player = player;
+                shot.HandleIncoming();
+                GameObjectList playershots = GameWorld.Find("playershot") as GameObjectList;
+                playershots.Add(shot);
+            }
+        }
 
         //enemy shots updated
 
 
 
+    }
+
+    public void PlayerUpdate(Player orgP, Player newP)
+    {
+        orgP.health = newP.health;
+        orgP.keys = newP.keys;
+        orgP.potions = newP.Potions;
+        orgP.score = newP.score;
+        orgP.Mirror = newP.Mirror;
+        orgP.Position = newP.Position;
+        orgP.Velocity = newP.Velocity;
+        orgP.Visible = newP.Visible;
     }
 }
 
